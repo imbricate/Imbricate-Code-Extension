84b6bf2f4bfefe9e8b63b2b8a84946481264b81f
wmxpy <wm@mengw.io>
2024-05-22 01:26:12 -0500

    feat: Add script show history command

---
+++ src/command/script-show-history.ts
@@ -0,0 +1,33 @@
+/**
+ * @author WMXPY
+ * @namespace Command
+ * @description Script Show History
+ */
+
+import { IImbricateScript } from "@imbricate/core";
+import * as vscode from "vscode";
+import { HistoryTreeViewDataProvider } from "../history-tree-view/data-provider";
+import { ScriptScriptItem } from "../scripts-tree-view/script-item";
+import { showErrorMessage } from "../util/show-message";
+
+export const registerScriptShowHistoryCommand = (
+    historiesDataProvider: HistoryTreeViewDataProvider,
+): vscode.Disposable => {
+
+    const disposable = vscode.commands.registerCommand("imbricate.script.show-history", async (
+        item: ScriptScriptItem,
+    ) => {
+
+        const script: IImbricateScript | null =
+            await item.origin.getScript(item.scriptSnapshot.identifier);
+
+        if (!script) {
+            showErrorMessage(`Cannot find script: ${item.scriptSnapshot.scriptName}`);
+            return;
+        }
+
+        historiesDataProvider.showRecords(script.historyRecords);
+    });
+
+    return disposable;
+};
