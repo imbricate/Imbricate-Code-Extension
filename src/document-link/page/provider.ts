e7cf29a09d99d2555310075febe98ed8c5a87269
wmxpy <wm@mengw.io>
2024-06-15 23:37:42 -0500

    feat: Add PageDocumentLinkProvider for markdown files

--- src/document-link/page/provider.ts
+++ src/document-link/page/provider.ts
@@ -5,8 +5,9 @@
  */
 
 import * as vscode from "vscode";
+import { concatPageMarkdownUrl } from "../../virtual-document/page-markdown/concat";
 
-export class PageDocumentLinkProvider implements vscode.DocumentLinkProvider {
+export class PageDocumentLinkProvider implements vscode.TextDocumentContentProvider, vscode.DocumentLinkProvider {
 
     public static create(): PageDocumentLinkProvider {
 
@@ -15,13 +16,33 @@ export class PageDocumentLinkProvider implements vscode.DocumentLinkProvider {
 
     private constructor() { }
 
-    public provideDocumentLinks(
-        document: vscode.TextDocument,
+    public provideTextDocumentContent(
+        uri: vscode.Uri,
         token: vscode.CancellationToken,
-    ): vscode.ProviderResult<vscode.DocumentLink[]> {
+    ): vscode.ProviderResult<string> {
 
-        console.log(document, token);
+        console.log(uri, token);
 
         throw new Error("Method not implemented.");
+    }
+
+    public provideDocumentLinks(
+        document: vscode.TextDocument,
+        _token: vscode.CancellationToken,
+    ): vscode.ProviderResult<vscode.DocumentLink[]> {
+
+        const range = new vscode.Range(
+            new vscode.Position(0, 0),
+            new vscode.Position(0, 5),
+        );
+
+        const uri = concatPageMarkdownUrl(
+            "item.originName",
+            "item.collection.uniqueIdentifier",
+            "item.pageSnapshot.identifier",
+        );
+
+        const documentLink = new vscode.DocumentLink(range, uri);
+        return [documentLink];
     }
 }
